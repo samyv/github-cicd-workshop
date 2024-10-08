880bb302d17b2412c10a0101ab6a5e40b2b713f8
Samy Van Vooren <20582544+samyv@users.noreply.github.com>
2024-10-08 09:00:22 +0000

    fix code again and again..

--- exercise_2/bad_code.py
+++ exercise_2/bad_code.py
@@ -4,6 +4,7 @@ def basic_math():
 # A comment that is so long, it is going to be split into multiple lines due
 # for general readability purposes
 
+
 if __name__ == "__main__":
     hello = "Hello, World!"
     basic_math()
